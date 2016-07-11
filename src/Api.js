78fe55b6b5bd79c2166b9e5698cd8007e0f03368
Pau Ramon Revilla <masylum@gmail.com>
2016-07-12 00:38:11 +0200

    :eggplant:

--- src/Api.js
+++ src/Api.js
@@ -6,8 +6,8 @@ type Request = {
   promise: Promise;
 }
 
-function ajax (url: string): Request {
-  const xhr = jq.ajax(url)
+function ajax (url: string, options: {}): Request {
+  const xhr = jq.ajax(url, options)
 
   const promise = new Promise((resolve, reject) => {
     xhr
@@ -32,6 +32,18 @@ class API {
 
   fetch (path: string = ''): Request {
     return ajax(`${this.basePath}${path}`)
+  }
+
+  post (path: string = '', data): Request {
+    return ajax(`${this.basePath}${path}`, {method: 'POST', data})
+  }
+
+  put (path: string = '', data): Request {
+    return ajax(`${this.basePath}${path}`, {method: 'PUT', data})
+  }
+
+  del (path: string = ''): Request {
+    return ajax(`${this.basePath}${path}`, {method: 'DELETE'})
   }
 }
 
