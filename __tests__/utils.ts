02a4a1af492af293e90dc5f89329a3648b8abcab
Gerard Clos <clos.gerard@gmail.com>
2019-05-20 15:05:33 +0200

    target ES5

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,9 +1,7 @@
 export function strMapToObj(strMap) {
-    let obj = Object.create(null);
-    for (let [k,v] of strMap) {
-        // We don’t escape the key '__proto__'
-        // which can cause problems on older engines
-        obj[k] = v;
-    }
-    return obj;
+    let obj = Object.create(null)
+
+    strMap.forEach((val, key) => obj[key] = val)
+
+    return obj
 }
