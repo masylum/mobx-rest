42356608830c7c5a1e5bcee25f7ebd174778ca3e
Jing Xu <jing.xu@rightcapital.com>
2021-05-14 11:11:17 +0800

    Fix the issue #101 caused by generated declaration files path changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,44 @@
-import typescript from 'rollup-plugin-typescript2'
-import resolve from 'rollup-plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2';
+import resolve from 'rollup-plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: './src/index.ts',
-  output: {
-    file: './lib/index.js',
-    format: 'cjs'
-  },
-  plugins: [
-    resolve(),
-    typescript()
-  ],
-  external: [
-    'lodash/compact',
-    'lodash/debounce',
-    'lodash/difference',
-    'lodash/includes',
-    'lodash/isEqual',
-    'lodash/isObject',
-    'lodash/isPlainObject',
-    'lodash/union',
-    'lodash/uniqueId',
-    'lodash/intersection',
-    'lodash/entries',
-    'deepmerge',
-    'mobx'
-  ]
-}
+export default [
+	{
+		input: './src/index.ts',
+		output: {
+			file: './lib/index.js',
+			format: 'cjs'
+		},
+		plugins: [
+			resolve(),
+			typescript({
+				tsconfigOverride: {
+					compilerOptions: {
+						declaration: false
+					}
+				}
+			})
+		],
+		external: [
+			'lodash/compact',
+			'lodash/debounce',
+			'lodash/difference',
+			'lodash/includes',
+			'lodash/isEqual',
+			'lodash/isObject',
+			'lodash/isPlainObject',
+			'lodash/union',
+			'lodash/uniqueId',
+			'lodash/intersection',
+			'lodash/entries',
+			'deepmerge',
+			'mobx'
+		]
+	},
+	{
+		// Generate bundled declaration file
+		input: './src/index.ts',
+		output: [ { file: './lib/index.d.ts', format: 'es' } ],
+		plugins: [ dts() ]
+	}
+];
